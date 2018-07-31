c9336b8b6d7d56ad37d5e9958309b6f52b0e7487
Arno_2T <arnaud.tortora@gmail.com>
2018-07-31 16:04:33 +0200

    modal without style

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,9 +1,9 @@
 <template>
     <div id="backModal">
-         <h4>TiTrE</h4>
+         <h4>{{ movie.title }}</h4>
         <figure>
-            <img src="assets/img/starwars.jpg" />
-            <p>Synopsis</p>
+            <img :src="getImgUrl()" />
+            <p>{{ movie.synopsis }}</p>
             <button v-on:click="close">Close</button>
     </figure>
     </div>
@@ -11,10 +11,16 @@
 
 <script>
 export default {
+    props:{
+        movie:{type: Object, required:true}
+    },
     methods:{
         close(){
            return close= true
-        }
+        },
+         getImgUrl(){
+           return `assets/img/${this.movie.url}`
+       },
     }
 }
 </script>
